bf60fdac2e09c1502243dc4d351643f86f694b7a
Ricky Gilleland <ricky@strikr.co>
2020-03-16 22:56:45 -0400

    Change home header font size

--- resources/views/welcome-coming-soon.blade.php
+++ resources/views/welcome-coming-soon.blade.php
@@ -30,7 +30,7 @@
             </div>
         </nav>
         
-            <h1 class="text-center" style="font-size: 3.5rem;font-weight:600">Water Cooler Chat, Sans Water Cooler</h1>
+            <h1 class="text-center" style="font-size: 3.9rem;font-weight:600">Water Cooler Chat, Sans Water Cooler</h1>
             <p class="text-center" style="font-weight:400;font-size:1.4rem">We give your remote team a place to feel a little less remote because some of the best ideas come from casual conversation, not meetings. Close your eyes and you just might forget you aren't in the office. </p>
             <p class="text-center" style="font-weight:400;font-size:1.3rem">Starting at only $10/month for up to 5 team members with a 30 day free trial.</p>
 

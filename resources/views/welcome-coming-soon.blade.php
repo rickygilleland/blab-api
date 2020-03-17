2d5a404b883c66e88fff22763290d046505fb231
Ricky Gilleland <ricky@strikr.co>
2020-03-16 22:55:07 -0400

    Add coming soon form to home page Change url for login page to prevent people from finding it

---
+++ resources/views/welcome-coming-soon.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Water Cooler</title>
+
+        <script src="/js/app.js" defer></script>
+
+        <link href="/css/app.css" rel="stylesheet">
+
+        <script src="https://kit.fontawesome.com/584495cc88.js"></script>
+
+    </head>
+    <body>
+
+        <div class="container">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"><img src="img/water_cooler.png" class="img img-fluid" style="height:60px"> <span style="font-weight:900;font-size:1.5rem;">Water Cooler</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+
+                </ul>
+            </div>
+        </nav>
+        
+            <h1 class="text-center" style="font-size: 3.5rem;font-weight:600">Water Cooler Chat, Sans Water Cooler</h1>
+            <p class="text-center" style="font-weight:400;font-size:1.4rem">We give your remote team a place to feel a little less remote because some of the best ideas come from casual conversation, not meetings. Close your eyes and you just might forget you aren't in the office. </p>
+            <p class="text-center" style="font-weight:400;font-size:1.3rem">Starting at only $10/month for up to 5 team members with a 30 day free trial.</p>
+
+            <center><a class="btn btn-primary btn-lg text-light" href="https://share.hsforms.com/1Fg-icSYPTuOAmDM7GDqPug46cuj">Let Me Know When It's Ready</a></center>
+        </div>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome-coming-soon');
 });
 
 
 Route::get('login/slack/callback', 'Auth\LoginController@handleSlackCallback');
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('login-tucker', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/home', 'HomeController@index')->name('home');

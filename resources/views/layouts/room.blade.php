483b848cbb7a0b53b81cb004b3032f3053df240c
Ricky Gilleland <ricky@strikr.co>
2020-03-18 22:40:46 -0400

    Remove menu button that doesn't do anything

--- resources/views/layouts/room.blade.php
+++ resources/views/layouts/room.blade.php
@@ -46,10 +46,7 @@
                 @else
                     <p style="color:white" class="ml-3 pt-2">{{ $team->slug }} / {{ $room->slug }}</p>
                 @endif
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
--- resources/views/welcome-coming-soon.blade.php
+++ resources/views/welcome-coming-soon.blade.php
@@ -24,9 +24,6 @@
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#"><img src="img/water_cooler.png" class="img img-fluid" style="height:60px"> <span style="font-weight:900;font-size:1.5rem;">Water Cooler</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">

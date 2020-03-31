54272cb319bd95dd728e2820d3ac8dcf7f1ccd6e
Ricky Gilleland <ricky@strikr.co>
2020-03-31 19:09:51 -0400

    Attach nts token to user request

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login/slack', 'Auth\LoginController@apiHandleSlackCallback');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/user', 'UserController@show');
 
 Route::middleware('auth:api')->get('/organization', 'OrganizationController@api_show');

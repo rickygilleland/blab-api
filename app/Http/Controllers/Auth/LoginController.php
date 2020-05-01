9c3e6a234f4ad4c14e637d76fac1198816c31a1d
Ricky Gilleland <ricky@strikr.co>
2020-04-30 23:22:09 -0400

    Fix class does not exist error

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 use Socialite;

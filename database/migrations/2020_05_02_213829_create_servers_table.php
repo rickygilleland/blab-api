e5ebea8dda02b3c34eeaecbc42c6c1d1f7fe39af
Ricky Gilleland <ricky@watercooler.work>
2020-05-02 17:53:30 -0400

    Add server model and migrations

---
+++ database/migrations/2020_05_02_213829_create_servers_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->id();
+            $table->string("hostname");
+            $table->string("location");
+            $table->boolean("is_active")->default(true);
+            $table->timestamps();
+        });
+
+        DB::table('servers')->insert([
+            'hostname' => 'streamer-us-west-1.watercooler.work',
+            'location' => 'us-west',
+            'is_active' => true
+        ]);
+
+        DB::table('servers')->insert([
+            'hostname' => 'streamer-us-west-2.watercooler.work',
+            'location' => 'us-west',
+            'is_active' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servers');
+    }
+}

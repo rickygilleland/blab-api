e7faa9e0bb8542e08d96b135b335b1e8667c8305
Ricky Gilleland <ricky@watercooler.work>
2020-05-07 23:00:42 -0400

    Update coming soon page with additional copy and screenshots

--- resources/views/welcome-coming-soon.blade.php
+++ resources/views/welcome-coming-soon.blade.php
@@ -34,10 +34,36 @@
         
             <h1 class="text-center" style="font-size: 3.9rem;font-weight:600">Give Your Distributed Team a Virtual Office</h1>
             <p class="text-center mt-3" style="font-weight:400;font-size:1.6rem">We give your team an always available place to chat that encourages natural conversation you would normally only get in a phyiscal office.</p>
-            <p class="text-center mt-3" style="font-weight:400;font-size:1.2rem">Water Cooler designed to run in the background and let you focus on communicating with your team while getting work done, instead of endlessly staring at our app. We're invite-only for now, but a public beta is coming soon.</p>
-            <center><img src="/img/main-hero-screenshot.png" class="img img-fluid"></center>
+            <p class="text-center mt-3" style="font-weight:400;font-size:1.2rem">Water Cooler designed to run in the background and let you focus on communicating with your team while getting work done, instead of endlessly staring at our app. Available in a limited, private beta.</p>
+            <center><a class="btn btn-primary btn-lg text-light my-4" href="https://share.hsforms.com/1Fg-icSYPTuOAmDM7GDqPug46cuj">Request an Invite</a></center>
+            <center><img src="/img/main-hero-screenshot.png" class="img img-fluid rounded"></center>
 
-            <p class="text-center mt-3" style="font-weight:400;font-size:1.4rem">Interested in being one of the first to try Water Cooler? Click the button below to request an invite.</p>
+            <hr class="my-5">
+            <div class="row mb-5 clearfix">
+                <div class="col-md-6">
+                    <h3 class="mt-4 h2">Always Available for Quick Meetings and Audio Only by Default</h3>
+                    <p class="lead">Create rooms that are available to the entire team, or restricted to a few select team members. Great for general office chit-chat, recurring meetings, or 1:1 conversations with your co-workers.</p>
+                    <p class="lead font-weight-bold">Rooms are always available and never require a special code to join or for a specific host to be present.</p>
+                    <p class="lead">We let Water Cooler fade in the background and put your team's conversations at the center of attention by making video chat optional and off by default in rooms.</p>                
+                </div>
+                <div class="col-md-6">
+                    <img src="/img/rooms-screenshot.png" style="max-height:400px" class="img img-fluid rounded float-right" />
+                </div>
+            </div>
+
+            <h3 class="h1 text-center" style="margin-top:6rem">Keep Track of Who's Available Across Multiple Timezones</h3>
+            <p class="lead text-center">We make it simple to see who's online and who might be available soon by showing everyone's local time. And when you're in a room, you'll see each members local time if it doesn't match your own. Don't worry about accidentally saying "good morning" when it's 3:30PM somewhere else.</p>
+
+            <img src="/img/team-screenshot.png" class="img img-fluid rounded" />
+
+            <h3 class="h1 text-center mt-5">Water Cooler is Cross Platform</h3>
+            <p class="lead text-center">Work without limitations.</p>
+            <p class="text-center" style="font-size:6.5rem"><i class="fab fa-apple"></i> <i class="fab fa-windows mx-5"></i> <i class="fab fa-linux"></i></p>
+            <p class="small text-muted text-center">Linux Support Coming Soon</p>
+
+            <hr class="my-5">
+
+            <p class="text-center" style="font-weight:400;font-size:1.4rem;">Interested in being one of the first to try Water Cooler for free?<br> Click the button below to request an invite.</p>
             <center><a class="btn btn-primary btn-lg text-light mt-3 mb-5" href="https://share.hsforms.com/1Fg-icSYPTuOAmDM7GDqPug46cuj">Request an Invite</a></center>
            
     

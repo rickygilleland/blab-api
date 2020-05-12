c9ecb83aa9fe0577de1add48607a4c92aca80c72
Ricky Gilleland <ricky@watercooler.work>
2020-05-12 00:40:22 -0400

    Add pricing section to home page Add content to the navbar

--- resources/views/welcome-coming-soon.blade.php
+++ resources/views/welcome-coming-soon.blade.php
@@ -24,11 +24,20 @@
             <div style="max-width:1400px" class="mx-auto pb-3">
 
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <a class="navbar-brand" href="#"><img src="img/water_cooler.png" class="img img-fluid" style="height:60px"></a>
+                    <a class="navbar-brand" href="#" style="font-weight:900;font-size:1.75rem;color:#fff!important"><img src="img/water_cooler.png" class="img img-fluid" style="height:60px">Water Cooler</a>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ml-auto">
-
+                            <li class="nav-item">
+                                <a class="nav-link text-light font-weight-bold pr-5" style="padding-top:1.35rem;font-size:1.1rem" href="#pricing"><i class="fas fa-tags"></i> Pricing</a>
+                            </li>   
+                            <li class="nav-item">
+                                <a class="btn btn-success shadow text-dark my-3 font-weight-bold" target="_blank" href="https://share.hsforms.com/1Fg-icSYPTuOAmDM7GDqPug46cuj">Request an Invite</a>
+                            </li>
                         </ul>
                     </div>
                 </nav>
@@ -39,7 +48,7 @@
                         <h1 class="text-center align-middle mt-md-5" style="font-weight:900;">Bring your team together with Water Cooler</h1>
                         <h2 class="text-center mt-4" style="font-weight:600;font-size:1.55rem">Stop the endless meetings and let your team work like they would physically, virtually.</h2>
                         <h3 class="text-center mt-3" style="font-weight:500;font-size:1.4rem">Rooms are always available, no codes and no host requirements. Now in a free beta.</h3>
-                        <center><a class="btn btn-success shadow btn-lg text-dark my-3 font-weight-bold" href="https://share.hsforms.com/1Fg-icSYPTuOAmDM7GDqPug46cuj">Request an Invite</a></center>
+                        <center><a class="btn btn-success shadow btn-lg text-dark my-3 font-weight-bold" target="_blank" href="https://share.hsforms.com/1Fg-icSYPTuOAmDM7GDqPug46cuj">Request an Invite</a></center>
                     </div>
                     <div class="col-md-7 order-sm-1">
                         <img src="/img/main-hero-screenshot.png" class="img img-fluid rounded">
@@ -63,9 +72,19 @@
                         <img src="/img/rooms-screenshot.png" style="max-height:400px" class="img img-fluid rounded shadow float-right" />
                     </div>
                 </div>
+            </div>
+        </div>
 
-                <hr class="my-5"/>
-
+        <div class="container-fluid" style="background-color:#3777ff;color:white" id="pricing">
+            <div style="max-width:1400px" class="mx-auto py-5">
+                <h3 class="h1 text-center mt-5 mb-3" style="font-weight:800"><i class="fas fa-tags"></i> Water Cooler Pricing</h3>
+                <p class="lead text-center mb-1" style="font-weight:500;font-size:1.45rem">Water Cooler is currently in a private beta and available completely free of charge.</p>
+                <p class="text-center pt-0 mb-5" style="font-size:1.1rem">Once Water Cooler is publicly released, there will be a free tier along with two to be announced paid tiers, charged on a per user basis.</p>
+            </div>
+        </div>
+                
+        <div class="container-fluid" style="background-color:#fff;color:black">
+            <div style="max-width:1400px" class="mx-auto py-5">
                 <h3 class="h1 text-center mt-5" style="font-weight:800">Water Cooler is Cross Platform</h3>
                 <p class="lead text-center" style="font-weight:500">Work without limitations.</p>
                 <p class="text-center" style="font-size:6.5rem"><i class="fab fa-apple"></i> <i class="fab fa-windows mx-5"></i> <i class="fab fa-linux"></i></p>
@@ -77,7 +96,7 @@
         <div class="container-fluid" style="background-color:#3777ff;color:white">
             <div style="max-width:1400px" class="mx-auto py-5">
                 <p class="text-center" style="font-weight:600;font-size:1.4rem;">Interested in being one of the first to try Water Cooler for free?<br> Click the button below to request an invite.</p>
-                <center><a class="btn btn-success shadow text-dark btn-lg mt-3 font-weight-bold" href="https://share.hsforms.com/1Fg-icSYPTuOAmDM7GDqPug46cuj">Request an Invite</a></center>
+                <center><a class="btn btn-success shadow text-dark btn-lg mt-3 font-weight-bold" target="_blank" href="https://share.hsforms.com/1Fg-icSYPTuOAmDM7GDqPug46cuj">Request an Invite</a></center>
             </div>
         </div>
 

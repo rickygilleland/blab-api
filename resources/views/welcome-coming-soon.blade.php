6d37c36f5b4ec982058583e541add93a3f5f6496
Ricky Gilleland <ricky@watercooler.work>
2020-05-12 23:47:25 -0400

    Add additional product details to home page Add info about security and e2ee Remove unused screenshots

--- resources/views/welcome-coming-soon.blade.php
+++ resources/views/welcome-coming-soon.blade.php
@@ -17,6 +17,10 @@
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
 
+        <!-- Start of HubSpot Embed Code -->
+            <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7015051.js"></script>
+        <!-- End of HubSpot Embed Code -->
+
     </head>
     <body>
 
@@ -32,6 +36,9 @@
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link text-light font-weight-bold pr-5" style="padding-top:1.35rem;font-size:1.1rem" href="#security"><i class="fas fa-shield-alt"></i> Security</a>
+                            </li>  
                             <li class="nav-item">
                                 <a class="nav-link text-light font-weight-bold pr-5" style="padding-top:1.35rem;font-size:1.1rem" href="#pricing"><i class="fas fa-tags"></i> Pricing</a>
                             </li>   
@@ -63,15 +70,39 @@
             
                 <div class="row mb-5 clearfix">
                     <div class="col-md-6">
-                        <h3 class="mt-4 h2" style="font-weight:800">Always Available for Quick Meetings and Audio Only by Default</h3>
+                        <h3 class="h2" style="font-weight:800;margin-top:5rem">Always Available, No Codes Required</h3>
                         <p class="lead" style="font-weight:500">Create rooms that are available to the entire team, or restricted to a few select team members. Great for general office chit-chat, recurring meetings, or 1:1 conversations with your co-workers.</p>
-                        <p class="lead font-weight-bold">Rooms are always available and never require a special code to join or for a specific host to be present.</p>
-                        <p class="lead" style="font-weight:500">We let Water Cooler fade in the background and put your team's conversations at the center of attention by making video chat optional and off by default in rooms.</p>                
+                        <p class="lead font-weight-bold">Rooms are always available and never require a special code to join or for a specific host to be present.</p>           
                     </div>
                     <div class="col-md-6">
                         <img src="/img/rooms-screenshot.png" style="max-height:400px" class="img img-fluid rounded shadow float-right" />
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="container-fluid" style="background-color:#3777ff;color:white">
+            <div style="max-width:1400px" class="mx-auto py-5">
+                <div class="row mb-5 clearfix">
+                    <div class="col-md-6">
+                        <h3 class="h2" style="font-weight:800;margin-top:5rem">Secure HD Video and Audio</h3>
+                        <p class="lead" style="font-weight:500">Connect with your team members with <a href="#security" style="color:#fff!important;font-weight:600">secure</a> HD video and audio chat available in every room (additional security details below).</p>
+                        <p class="lead" style="font-weight:500">All rooms are audio only by default, putting the conversation at center stage and reducing distractions and embarrassing moments.</p> 
+                        <p class="small">*Video quality and resolution may be reduced at times depending on network conditions.</p>             
+                    </div>
+                    <div class="col-md-6">
+                        <img src="/img/video-audio-containers-screenshot.png" style="max-height:400px" class="img img-fluid rounded shadow float-right" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="container-fluid" style="background-color:#fff;color:black" id="security">
+            <div style="max-width:1400px" class="mx-auto py-5">
+                <h3 class="h1 text-center mt-5 mb-3" style="font-weight:800"><i class="fas fa-shield-alt"></i> Secure and Optimized for Low-Latency</h3>
+                <p class="lead text-center mb-3" style="font-weight:500;font-size:1.3rem">Our video and audio chat is powered by WebRTC and all traffic between our desktop app and our servers is encrypted and secure, with <strong>full end-to-end encryption coming later this year.</strong></p>
+                <p class="lead text-center mb-3" style="font-weight:500;font-size:1.2rem">We designed our server infrastructure with reliability and scalability in mind. Water Cooler has servers on both coasts of the United States (with international expansion coming soon) and will automatically attempt to route your team to the nearest server for high-quality low-latency connections.</p>
             </div>
         </div>
 
@@ -86,7 +117,7 @@
         <div class="container-fluid" style="background-color:#fff;color:black">
             <div style="max-width:1400px" class="mx-auto py-5">
                 <h3 class="h1 text-center mt-5" style="font-weight:800">Water Cooler is Cross Platform</h3>
-                <p class="lead text-center" style="font-weight:500">Work without limitations.</p>
+                <p class="lead text-center" style="font-weight:500">Available on Windows, MacOS and will soon support Linux.</p>
                 <p class="text-center" style="font-size:6.5rem"><i class="fab fa-apple"></i> <i class="fab fa-windows mx-5"></i> <i class="fab fa-linux"></i></p>
                 <p class="small text-muted text-center">Linux Support Coming Soon</p>
 

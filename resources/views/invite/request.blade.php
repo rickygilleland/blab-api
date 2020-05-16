8108a5ff1b80aef27bffe0a8eeaf377ba3b53eed
Ricky Gilleland <ricky@watercooler.work>
2020-05-15 23:12:33 -0400

    Update request invite page

--- resources/views/invite/request.blade.php
+++ resources/views/invite/request.blade.php
@@ -8,20 +8,23 @@
                 <div class="card-body p-5">
 
                     <h2 class="pb-2 text-center">Request an Invitation to Water Cooler</h2>	
+                    <p class="text-center lead">Enter your name and email address and we'll send you an invitation to try Water Cooler soon.</p>
                     <hr>
 
                     @if (isset($success))
                         <div class="alert alert-success">
-                            <p class="text-center font-weight bold">Thank you! We'll send you an email as soon as you reach the front of the line. We're sending out invitations on a rolling basis every day depending on demand.</p>
+                            <p class="text-center font-weight-bold">Thank you! We'll send you an email as soon as you reach the front of the line.</p>
+                            <p class="text-center">We're sending out invitations on a rolling basis every day depending on demand, so keep an eye out for your invite!.</p>
                         </div>
                     @else
 
                         <form method="POST" action="/invite">
                             @csrf
-
+                            <label>First Name</label>
                             <input type="text" name="name" placeholder="First Name" class="form-control dark-input mb-4 py-4 shadow-sm" required/>
+                            <label>Email Address</label>
                             <input type="text" name="email" placeholder="Email" class="form-control dark-input mb-4 py-4 shadow-sm" required/>
-                            <button type="submit" class="btn btn-primary btn-block">Request Invite</button>
+                            <button type="submit" class="btn btn-primary btn-block btn-lg">Request Invite</button>
                         
                         </form>
                     @endif

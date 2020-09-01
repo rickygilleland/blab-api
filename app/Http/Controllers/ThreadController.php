04d510525e4180763a4a8ce3f2ef61b5c64df275
Ricky Gilleland <ricky@watercooler.work>
2020-09-01 00:17:01 -0400

    Build thread name on the fly with participants when name is null

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -9,12 +9,50 @@ class ThreadController extends Controller
     public function get_user_threads(Request $request) 
     {
         $user = \Auth::user();
+
+        $threads = [];
+        foreach ($user->threads as $thread) {
+            if ($thread->name == null) {
+                $name = '';
+
+                $i = 0;
+                foreach ($thread->users as $user) {
+                    $name .= $user->first_name;
+
+                    if ($i != count($thread->users) - 1) {
+                        $name .= ", ";
+                    }
+
+                    $i++;
+                }
+
+                $thread->name = $name;
+            }
+        }
         return $user->threads;
     }
 
     public function get_thread(Request $request, $id) 
     {
         $thread = \App\Thread::where('id', $id)->first();
+
+        if ($thread->name == null) {
+            $name = '';
+
+            $i = 0;
+            foreach ($thread->users as $user) {
+                $name .= $user->first_name;
+
+                if ($i != count($thread->users) - 1) {
+                    $name .= ", ";
+                }
+                
+                $i++;
+            }
+
+            $thread->name = $name;
+        }
+
         return $thread;
     }
 

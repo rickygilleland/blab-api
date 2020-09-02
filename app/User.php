ec847cc06ff8c92f39ab76c039bce08d1ad2b5fd
Ricky Gilleland <ricky@watercooler.work>
2020-09-01 20:51:41 -0400

    Add streamer_key to list of hidden user fields on user model

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'provider', 'provider_id', 'created_at', 'email_verified_at',
+        'password', 'remember_token', 'provider', 'provider_id', 'created_at', 'email_verified_at', 'streamer_key',
     ];
 
     /**

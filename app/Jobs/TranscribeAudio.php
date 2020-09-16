0e16fea43e27b73131302cdb4032cd9a950cb5c3
Ricky Gilleland <ricky.gilleland@gmail.com>
2020-09-16 08:28:20 -0400

    Add release phase script that downloads deepspeech model files Add start of TranscribeAudio job

---
+++ app/Jobs/TranscribeAudio.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+use App\Events\DirectMessageUpdated;
+use TitasGailius\Terminal;
+
+class TranscribeAudio implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        $audio = Storage::disk('spaces')->get($this->message->attachment_path);
+
+        //write the raw audio content to /tmp
+        $dir = sys_get_temp_dir();
+        $audio_tmp_file = tempnam($dir, "audio_transcribe_");
+        file_put_contents($audio_tmp_file, $audio);
+
+        $transcribe_result = Terminal::run('deepspeech --model deepspeech-0.8.2-models.pbmm --scorer deepspeech-0.8.2-models.scorer --audio ' . $audio_tmp_file);
+        
+        //clean up the tmp file
+        unlink($audio_tmp_file);
+        
+    }
+}
---
+++ release-tasks.sh
@@ -0,0 +1,3 @@
+# Download the current deepspeech model files
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.8.2/deepspeech-0.8.2-models.pbmm
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.8.2/deepspeech-0.8.2-models.scorer
